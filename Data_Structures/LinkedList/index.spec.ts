357b5de7df397a9d70336d6567db7051cc20ac7b
marrionluaka <marrion91300@gmail.com>
2019-03-03 23:19:36 -0800

    Adding LinkedList structure

---
+++ Data_Structures/LinkedList/index.spec.ts
@@ -0,0 +1,159 @@
+import { expect } from 'chai';
+import { LinkedList } from '.';
+
+describe('LinkedList Spec', () => {
+	
+    context("LinkedList Creation", () => {
+        let value: string, sut: LinkedList<string>;
+      
+      beforeEach(() => {
+            value = "Hello";
+            sut = new LinkedList();
+            sut.Clear();
+        });
+      
+      it('adds a new item', () => {
+        // Act
+        const expected = 1;
+        const actual = sut.Append(value).Count();
+        
+        // Assert
+        expect(actual).to.equal(expected);
+        expect(sut.GetHead().value).to.equal(value);
+      });
+      
+      it('adds a new item to list when list is not empty', () => {
+          // Act
+        const expected = "World";
+        sut.Append(value).Append("World");
+  
+        // Assert
+        expect(sut.GetHead().next.value).to.equal(expected);
+      });
+      
+      it('returns null when the position is less than 1', () => {
+          // Act
+        const expected = null;
+        const actual = sut.Append(value).RemoveAt(0);
+        
+        // Assert
+        expect(actual).to.equal(expected);
+      });
+      
+      it('returns null when the position is greater than the list\'s Count', () => {
+          // Act
+        const expected = null;
+        const actual = sut.Append(value).RemoveAt(10);
+        
+        // Assert
+        expect(actual).to.equal(expected);
+      });
+      
+      it('removes an item from a specified position in the list', () => {
+        // Act
+        const expected = "World";
+        const actual = sut.Append(value).Append("World").RemoveAt(2);
+        
+        // Assert
+        expect(actual).to.eql(expected);
+        expect(sut.GetHead().value).to.equal(value)
+      });
+      
+      it('returns null when the position is invalid', () => {
+        // Act
+        const actual = sut.Append(value).Insert(-1, "Good-Bye");
+        
+        // Assert
+        expect(actual).to.equal(false);
+      });
+      
+      it('returns null when the position is an out-of-bounds value', () => {
+        // Act
+        const actual = sut.Append(value).Insert(4, "Good-Bye");
+        
+        // Assert
+        expect(actual).to.equal(false);
+      });
+      
+      it('inserts an element at the begining of the list', () => {
+        // Act
+        sut.Append(value).Insert(1, "Good-Bye");
+        
+        // Assert
+        expect(sut.GetHead().value).to.equal("Good-Bye");
+      });
+      
+      it('inserts an element at a specified position', () => {
+        // Act
+        const actual = sut.Append(value).Append("World").Insert(2, "Good-Bye");
+        
+        // Assert
+        expect(actual).to.equal(true);
+        expect(sut.Count()).to.equal(3);
+        expect(sut.GetHead().next.value).to.equal("Good-Bye");
+      });
+      
+      it('returns the index of the element in the list', () => {
+        // Act
+        const expected = 1;
+        const actual = sut.Append(value).IndexOf(value);
+        
+        // Assert
+        expect(actual).to.eql(expected);
+      });
+      
+      it('returns a string representation of the current list', () => {
+        // Act
+        const expected = "Hello World";
+        const actual = sut.Append(value).Append("World").ToString();
+        
+        // Assert
+        expect(actual).to.eql(expected);
+      });
+      
+      it('returns the position of the specified element in the list', () => {
+        // Act
+        const expected = 2;
+        const actual = sut.Append(value).Append("World").IndexOf("World");
+        
+        // Assert
+        expect(actual).to.eql(expected);
+      });
+      
+      it('returns -1 when the given element is not in the list', () => {
+        // Act
+        const expected = -1;
+        const actual = sut.Append(value).Append("World").IndexOf("FLOP");
+        
+        // Assert
+        expect(actual).to.eql(expected);
+      });
+      
+      it('removes an element from the list based on its value', () => {
+        // Act
+        const expected = "World";
+        const actual = sut.Append(value).Append("World").Remove("World");
+        
+        // Assert
+        expect(actual).to.eql(expected);
+      });
+      
+      it('returns true when the list is empty', () => {
+        // Act
+        const actual = sut.IsEmpty();
+        
+        // Assert
+        expect(actual).to.equal(true);
+      });
+      
+      it('returns the first element in the list', () => {
+        // Act
+        const actual = sut.Append(value).GetHead();
+        
+        // Assert
+        expect(actual.value).to.equal(value);
+      });
+      
+    });
+    
+  });
---
+++ Data_Structures/LinkedList/index.ts
@@ -0,0 +1,130 @@
+import Node from "../Helpers/Node";
+
+export class LinkedList<T> {
+    private _head: Node<T>;
+    private _length: number;
+
+    constructor(){
+        this._length = 0;
+    }
+
+    public Append(value: any): LinkedList<T> {
+    	let node = new Node(value), current;
+      
+        if(!this._head){
+            this._head = node;
+        } else {
+            this._head = this._GetLastNode(this._head);
+            this._head.next = node;
+        }
+      
+        this._length++;
+        return this;
+    }
+    
+    public Insert(position: number, value: any): boolean {
+        if(position < 1 || position > this._length) return false;
+        
+        let node = new Node(value),
+            current = this._head,
+            index = 1;
+        
+        if(position === 1){
+            node.next = current;
+            this._head = node;
+        } else {
+            const item = this._FindItem(index, position, this._head);
+            node.next = item.current;
+            item.prev.next = node;
+        }
+        
+        this._length++;
+        return true
+    }
+    
+    public RemoveAt(position: number) {
+        if(position < 1 || position > this._length) return null;
+        
+        let current = this._head,
+            item,
+            index = 1;
+        
+        if(position === 1){
+            this._head = current.next;
+        } else {
+            item = this._FindItem(index, position, this._head);
+            item.prev.next = item.current.next
+        }
+        
+        this._length--;
+        return item.current.value;
+    }
+    
+    public ToString(): string{
+        let current = this._head,
+            res = '';
+        
+        while(current){
+            res += current.value + (current.next ? ' ' : '');
+            current = current.next;
+        }
+        return res;
+    }
+    
+    public IndexOf(value): number{
+        let current = this._head,
+            index = 1;
+            
+        while(current){
+            if(value === current.value) return index;
+        
+            index++;
+            current = current.next;
+        }
+        return -1;
+    }
+
+    public Remove(value){
+        return this.RemoveAt(this.IndexOf(value));
+    }
+    
+    public IsEmpty(){
+    	return this._length === 0;
+    }
+    
+    public Count(){
+    	return this._length;
+    }
+    
+    public GetHead(){
+    	return this._head;
+    }
+
+    public Clear(): void {
+        this._head = null;
+        this._length = 0;
+    }
+
+    private _GetLastNode(node: Node<T>): Node<T>{
+        let last = Object.assign({}, node);
+        
+        while(last.next){
+            last = last.next;
+        }
+        return last;
+    }
+    
+    private _FindItem(index: number, position: number, head: Node<T>){
+        let prev, current = head;
+
+        while(index++ < position){
+            prev = current;
+            current = current.next;
+        }
+
+        return {
+            prev,
+            current
+        };
+    }
+}
