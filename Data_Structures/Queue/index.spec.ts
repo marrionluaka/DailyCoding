0970ab80e3f4b8a69ab220c4ba74a5b4f63655c8
marrionluaka <marrion91300@gmail.com>
2019-03-10 00:04:50 -0800

    Adding queue data structure

---
+++ Data_Structures/Queue/index.spec.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import Queue from './';
+
+describe.only("Queue Spec", () => {
+    let sut: Queue<string>;
+    beforeEach(() => sut = new Queue());
+
+    it("Enqueue(): adds element to queue", () => {
+        sut.Enqueue("hello");
+        expect(sut.Count).to.equal(1);
+    });
+
+    it("Dequeue(): removes first element", () => {
+        sut.Enqueue("hello");
+
+        expect(sut.Dequeue()).to.equal("hello");
+        expect(sut.Count).to.equal(0);
+    });
+
+    it("Clear(): ", () => {
+        sut.Enqueue("hello");
+
+        sut.Clear();
+        expect(sut.Count).to.equal(0);
+        expect(sut.Front).to.be.null;
+    });
+});
---
+++ Data_Structures/Queue/index.ts
@@ -0,0 +1,55 @@
+import Node from '../Helpers/Node';
+
+export default class Queue<T> {
+    private _first: Node<T>;
+    private _last: Node<T>;
+    private _size: number;
+
+    constructor(){
+        this._size = 0;
+    }
+
+    public get Count(): number {
+        return this._size;
+    }
+
+    public get Front(): Node<T> {
+        return this._first
+    }
+
+    public get IsEmpty(): boolean {
+        return this.Count === 0;
+    }
+
+    public Enqueue(val: T): number {
+        const newNode = new Node(val);
+
+        if(!this._first){
+            this._first = newNode;
+            this._last = newNode;
+        } 
+        else {
+            this._last.next = newNode;
+            this._last = newNode;
+        }
+
+        return ++this._size;
+    }
+
+    public Dequeue(): T {
+        if(!this._first) return null;
+
+        const temp = this._first;
+        if(this._first === this._last) this._last = null;
+
+        this._first = this._first.next;
+        this._size--;
+        return temp.value;
+    }
+
+    public Clear(): void {
+        this._first = null;
+        this._last = null;
+        this._size = 0;
+    }
+}
