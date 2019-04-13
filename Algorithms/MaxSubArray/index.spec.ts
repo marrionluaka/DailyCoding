0f4685701707882bee6ab0527b8311456a5efbc1
marrionluaka <marrion91300@gmail.com>
2019-04-12 21:47:09 -0700

    Adding more test cases

--- Algorithms/MaxSubArray/index.spec.ts
+++ Algorithms/MaxSubArray/index.spec.ts
@@ -4,9 +4,11 @@ import maxSubarraySum from '.';
 describe('maxSubarraySum spec', () => {
     it('returns the max sum of a given array.', () => {
         const res1 = maxSubarraySum([100, 200, 300, 400], 2);
-        const res2 = maxSubarraySum([9, 45, 90, 17, 23], 2);
+        const res2 = maxSubarraySum([9, 45, 90, 17, 23], 4);
+        const res3 = maxSubarraySum([1,2], 3);
 
         expect(res1).to.equal(700);
-        expect(res2).to.equal(135);
+        expect(res2).to.equal(175);
+        expect(res3).to.be.null;
     });
 });
