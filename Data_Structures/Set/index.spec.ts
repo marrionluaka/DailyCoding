98ad6c93b6f45f1b08dba708d2d5bfd86425d5d5
marrionluaka <marrion91300@gmail.com>
2019-05-14 21:43:52 -0700

    Adding set Data Structure

---
+++ Data_Structures/Set/index.spec.ts
@@ -0,0 +1,134 @@
+import { expect } from 'chai';
+import { Set } from '.';
+
+
+describe.only('Set Spec', () => {
+    let sut: Set<string>;
+    const item = 'item';
+
+    beforeEach(() => {
+        sut = new Set()
+    });
+
+    it('add():', () => {
+        // Act
+        sut.add(item);
+
+        // Assert
+        expect(sut.size()).to.equal(1);
+    });
+
+    it('delete():', () => {
+        // Arrange
+        sut.add(item);
+        expect(sut.size()).to.equal(1);
+
+        // Act
+        sut.delete(item);
+
+        // Assert
+        expect(sut.size()).to.equal(0);
+
+    });
+
+    it('has():', () => {
+        // Arrange
+        expect(sut.has(item)).to.be.false;
+
+        // Act
+        sut.add(item)
+
+        // Assert
+        expect(sut.has(item)).to.be.true;
+    });
+
+    it('clear():', () => {
+        // Arrange
+        sut.add(item);
+
+        // Act
+        sut.clear();
+
+        // Assert
+        expect(sut.size()).to.equal(0);
+    });
+
+    it('size():', () => {
+        // Arrange
+        sut.add(item);
+
+        // Assert
+        expect(sut.size()).to.equal(1);
+    });
+
+    it('values():', () => {
+        // Arrange
+        sut.add(item);
+        sut.add('item2');
+
+        // Assert
+        expect(sut.values()).to.eql(['item', 'item2']);
+    });
+
+    context('Set operations', () => {
+        let otherSet: Set<string>;
+
+        beforeEach(() => {
+            otherSet = new Set();
+
+            otherSet.add('1');
+            otherSet.add('2');
+            otherSet.add('3');
+        });
+
+        it('union():', () => {     
+            // Arrange
+            sut.add('3');
+            sut.add('4');
+            sut.add('5');
+
+            // Act
+            const expected = ['1', '2', '3', '4', '5'];
+            const actual = sut.union(otherSet).values();
+
+            // Assert
+            expect(actual).to.eql(expected);
+        });
+
+        it('intersect():', () => {
+            // Arrange
+            sut.add('2');
+            sut.add('3');
+            sut.add('4');
+
+            // Act
+            const expected = ['2', '3'];
+            const actual = sut.intersect(otherSet).values();
+
+            // Assert
+            expect(actual).to.eql(expected);
+        });
+
+        it('difference():', () => {
+            // Arrange
+            
+
+            // Act
+            sut.difference(otherSet);
+
+            // Assert
+            
+        });
+
+        it('subset():', () => {
+            // Arrange
+            
+
+            // Act
+            sut.subset(otherSet);
+
+            // Assert
+            
+        });
+    });
+});
---
+++ Data_Structures/Set/index.ts
@@ -0,0 +1,65 @@
+export class Set<T> {
+
+    private _items: any
+
+    constructor(){
+        this._items = {}
+    }
+
+    public add(item: T): boolean {
+        if(this.has(item)) return false;
+
+        this._items[item] = item;
+        return true;
+    }
+
+    public delete(item: T): boolean {
+        if(!this.has(item)) return false;
+
+        delete this._items[item];
+        return true;
+    }
+
+    public values(): T[] {
+        return Object
+            .keys(this._items)
+            .reduce((values, val) =>  [...values, val], []);
+    }
+
+    public union(setB: Set<T>){
+        const unionSet = new Set();
+        const fn = (v: T) => unionSet.add(v);
+
+        this.values().forEach(fn);
+        setB.values().forEach(fn);
+        return unionSet;
+    }
+
+    public intersect(setB: Set<T>){
+        return this.values().reduce((intersection, val) =>{
+            if(setB.has(val))
+                intersection.add(val);
+            return intersection;
+        }, new Set());
+    }
+
+    public difference(set: Set<T>){
+        throw '';
+    }
+
+    public subset(set: Set<T>){
+        throw '';
+    }
+
+    public has(item: T): boolean {
+        return this._items.hasOwnProperty(item);
+    }
+
+    public size(): number {
+        return Object.keys(this._items).length
+    }
+
+    public clear(): void {
+        this._items = {}
+    }
+}
